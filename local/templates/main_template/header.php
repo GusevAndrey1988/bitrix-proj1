db4e6bd7858a480bf9df60d9818229ac0d24fbfc
Gusev Andrey <GusevAndrey1988@gmail.com>
2021-10-30 09:42:30 +0300

    script load fix

--- local/templates/main_template/header.php
+++ local/templates/main_template/header.php
@@ -20,14 +20,15 @@
         <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700,900" rel="stylesheet" />
         <link href="<?=\CUtil::GetAdditionalFileURL(SITE_TEMPLATE_PATH . '/assets/css/tooplate-style.css');?>" type="text/css" rel="stylesheet" />
 
+	<script src="<?=SITE_TEMPLATE_PATH?>/vendor/jquery/jquery.min.js"></script>
+	<script src="<?=SITE_TEMPLATE_PATH?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
         <?php
             $templateAsset->addCss(SITE_TEMPLATE_PATH . '/vendor/bootstrap/css/bootstrap.min.css');
             $templateAsset->addCss(SITE_TEMPLATE_PATH . '/assets/css/fontawesome.css');
             $templateAsset->addCss(SITE_TEMPLATE_PATH . '/assets/css/owl.css');
             $templateAsset->addCss(SITE_TEMPLATE_PATH . '/assets/css/lightbox.css');
  
-            $templateAsset->addJs(SITE_TEMPLATE_PATH . '/vendor/jquery/jquery.min.js', true);
-            $templateAsset->addJs(SITE_TEMPLATE_PATH . '/vendor/bootstrap/js/bootstrap.bundle.min.js');
             $templateAsset->addJs(SITE_TEMPLATE_PATH . '/assets/js/isotope.min.js');
             $templateAsset->addJs(SITE_TEMPLATE_PATH . '/assets/js/owl-carousel.js');
             $templateAsset->addJs(SITE_TEMPLATE_PATH . '/assets/js/lightbox.js');
